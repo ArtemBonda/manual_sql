bfd2cb5ce0df54608314bade0c15ffeefab69d8a
Artem B <garribonda@yandex.ru>
2022-07-28 10:16:07 +0300

    Изучение jsonb

--- docker-entrypoint-initdb.d/01_requests.sql
+++ docker-entrypoint-initdb.d/01_requests.sql
@@ -102,3 +102,101 @@ INSERT INTO seats (aircraft_code, seat_no, fare_conditions)
 -- UPDATE aircrafts
 --     SET range = range / 2
 --     WHERE aircraft_code = 'SU9';
+
+
+--  CHAPTER 4 --
+-- CREATE TABLE databases (
+--   is_open_source boolean ,
+--   dbms_name text
+-- );
+-- insert  into databases
+-- values (True, 'Postgres'),
+--        (0::boolean, 'Oracle'),
+--        (1::boolean, 'MySql'),
+--        (false, 'MS SQL');
+--
+-- SELECT * from databases WHERE is_open_source = false;
+
+-- Массивы
+CREATE TABLE pilots (
+  pilot_name text,
+  shedule integer[]
+);
+
+INSERT INTO pilots
+   VALUES ('Ivan', '{1, 3, 5, 6, 7}'),
+          ('Petr', '{1, 2, 5, 7}'),
+          ('Pavel', '{2, 5 }'),
+          ('Boris', '{3, 5, 6}');
+
+SELECT * FROM pilots;
+
+UPDATE pilots
+SET shedule = shedule || 7
+WHERE pilot_name = 'Boris';
+
+UPDATE pilots
+SET shedule = array_append(shedule, 6)
+WHERE pilot_name = 'Pavel';
+
+UPDATE pilots
+SET shedule = array_prepend(1, shedule)
+WHERE pilot_name = 'Pavel';
+
+UPDATE pilots
+SET shedule = array_remove(shedule, 5)
+WHERE pilot_name = 'Ivan';
+
+UPDATE pilots
+SET shedule[1] =2, shedule[2]=3
+WHERE pilot_name = 'Petr';
+
+UPDATE pilots
+SET shedule[1:2] = ARRAY[2,3]
+where pilot_name = 'Petr';
+
+SELECT * FROM pilots
+    WHERE array_position(shedule, 3) IS NOT NULL ;
+
+SELECT * FROM pilots
+    WHERE shedule @> '{1, 7}'::integer[];
+
+SELECT * FROM pilots
+WHERE shedule && ARRAY[2, 5];
+
+SELECT * FROM pilots
+WHERE NOT (shedule && ARRAY[2,5]);
+
+SELECT pilot_name, unnest(shedule) AS days_of_week
+    FROM pilots
+    ORDER BY  days_of_week;
+
+-- type JSON --
+
+CREATE TABLE pilot_hobbies(
+  pilot_name text,
+  hobbies jsonb
+);
+
+INSERT INTO pilot_hobbies
+    VALUES ('Ivan',
+            '{"sports":["футбол","плавиние"],
+                "home_lib": true, "trips":3
+}'::jsonb),
+        ('Petr',
+         '{"sports":["теннис", "плавание"],
+         "home_lin":true, "trips":2
+                  }'::jsonb),
+        ('Pavel',
+         '{"sports":["плавание"],
+           "home_lib": false, "trips": 4}'::jsonb),
+        ('Boris',
+         '{"sports":["футбол", "плавание", "теннис"],
+            "home_lib":true, "trips":0
+        }'::jsonb);
+
+SELECT * From pilot_hobbies
+    where hobbies @> '{"home_lib":true}'::jsonb;
+SELECT pilot_name, hobbies->'sports' AS sport
+    FROM pilot_hobbies
+    WHERE hobbies->'sports' @> '["футбол"]'::jsonb;
